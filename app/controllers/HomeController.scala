dd5d83e241dbfb75499db6c745b404f54666fb5b
Simon Forster <simon.forster98@gmail.com>
2022-03-27 22:00:47 +0100

    reversing last commit changes

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -20,7 +20,5 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
-
-    // test commit
   }
 }
